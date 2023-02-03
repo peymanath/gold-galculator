6c702d82ac484cef99daad75d748f9c04c4c7f37
Peyman (ATH) Naderi <peymanath@gmail.com>
2023-02-03 03:49:56 -0800

    added property clss & change funstion translate number

--- public/js/calculatorGold.js
+++ public/js/calculatorGold.js
@@ -3,60 +3,111 @@
  * @version 1.1.0
  * {@link https://github.com/peymanath github}
  */
-function str_replace(search, replace, subject, countObj) {
-	var i = 0;
-	var j = 0;
-	var temp = "";
-	var repl = "";
-	var sl = 0;
-	var fl = 0;
-	var f = [].concat(search);
-	var r = [].concat(replace);
-	var s = subject;
-	var ra = Object.prototype.toString.call(r) === "[object Array]";
-	var sa = Object.prototype.toString.call(s) === "[object Array]";
-	s = [].concat(s);
-
-	var $global = typeof window !== "undefined" ? window : global;
-	$global.$locutus = $global.$locutus || {};
-	var $locutus = $global.$locutus;
-	$locutus.php = $locutus.php || {};
-
-	if (typeof search === "object" && typeof replace === "string") {
-		temp = replace;
-		replace = [];
-		for (i = 0; i < search.length; i += 1) {
-			replace[i] = temp;
-		}
-		temp = "";
-		r = [].concat(replace);
-		ra = Object.prototype.toString.call(r) === "[object Array]";
-	}
-
-	if (typeof countObj !== "undefined") {
-		countObj.value = 0;
-	}
-
-	for (i = 0, sl = s.length; i < sl; i++) {
-		if (s[i] === "") {
-			continue;
-		}
-		for (j = 0, fl = f.length; j < fl; j++) {
-			temp = s[i] + "";
-			repl = ra ? (r[j] !== undefined ? r[j] : "") : r[0];
-			s[i] = temp.split(f[j]).join(repl);
-			if (typeof countObj !== "undefined") {
-				countObj.value += temp.split(f[j]).length - 1;
-			}
-		}
-	}
-	return sa ? s : s[0];
-}
 
 /**
  * the main class for calculating gold
  */
 class GoldCalculator {
+	/**
+	 * output default value
+	 * @type {Number}
+	 */
+	salesPrice = 0;
+
+	/**
+	 * output default value
+	 * @type {Number}
+	 */
+	goldPrice = 0;
+
+	/**
+	 * output default value
+	 * @type {Number}
+	 */
+	grantPrice = 0;
+
+	/**
+	 * output default value
+	 * @type {Number}
+	 */
+	profitPrice = 0;
+
+	/**
+	 * output default value
+	 * @type {Number}
+	 */
+	taxPrice = 0;
+
+	/**
+	 * output default value
+	 * @type {Number}
+	 */
+	otherPrice = 0;
+
+	/**
+	 * Receive Element
+	 * @type {Element}
+	 */
+	weightInput = this.getElement("weight-input", "value");
+
+	/**
+	 * Receive Element
+	 * @type {Element}
+	 */
+	goldDailyRateInput = this.getElement("gold-daily-rate-input", "value");
+
+	/**
+	 * Receive Element
+	 * @type {Element}
+	 */
+	salesProfitInput = this.getElement("sales-profit-input", "value");
+
+	/**
+	 * Receive Element
+	 * @type {Element}
+	 */
+	discountInput = this.getElement("discount-input", "value");
+
+	/**
+	 * Receive Element
+	 * @type {Element}
+	 */
+	taxInput = this.getElement("tax-input", "value");
+
+	/**
+	 * Receive Element
+	 * @type {Element}
+	 */
+	constructionWagesInput = this.getElement("construction-wages-input", "value");
+
+	/**
+	 * Receive Element
+	 * @type {Element}
+	 */
+	constructionWagesPercentInput = this.getElement("construction-wages-percent-input", "value");
+
+	/**
+	 * Receive Element
+	 * @type {Element}
+	 */
+	jewelValueInput = this.getElement("jewel-value-input", "value");
+
+	/**
+	 * Receive Element
+	 * @type {Element}
+	 */
+	valueStoneInput = this.getElement("value-stone-input", "value");
+
+	/**
+	 * Receive Element
+	 * @type {Element}
+	 */
+	valueOtherBelongingsInput = this.getElement("value-other-belongings-input", "value");
+
+	constructor() {
+		
+	}
+
 	/**
 	 * get elemnts from the document
 	 * @param {String} id => the id Element
@@ -91,16 +142,19 @@ class GoldCalculator {
 	 * @param {String} language => Language List {'fa', 'en'}
 	 * @returns
 	 */
-	translateNumber = (number, language) => {
-		if (typeof number != "string") return new Error("not defined string").message;
-		if (language === "fa") number = number.replace(/\d/g, d => "۰۱۲۳۴۵۶۷۸۹"[d]);
-		if (language === "en") number = number.replace(/[۰-۹]/g, d => "۰۱۲۳۴۵۶۷۸۹".indexOf(d));
-		return number;
-	};
+	translateNumber(number, language = 'fa') {
+		if (typeof number !== "string") number = String(number);
+
+		const translate = num => {
+            // Persian Translate
+			if (language === "fa") return num.replace(/\d/g, d => "۰۱۲۳۴۵۶۷۸۹"[d]);
+            
+            // English Translate
+			if (language === "en") return num.replace(/[۰-۹]/g, d => "۰۱۲۳۴۵۶۷۸۹".indexOf(d));
+		};
+
+		return translate(number);
+	}
 }
 
 const gold = new GoldCalculator();
-
-console.log(gold.getElement("gold-daily-rate-input", "value"));
-console.log(gold.getElement("sales-profit-input", "value"));
-console.log(gold.toNumber("13,45,18.45", "fa"));
